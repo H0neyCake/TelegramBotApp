f31754e398ab10e9e82024c474817ed69d7c9b62
Cake <dimex4@ya.ru>
2018-03-27 05:22:47 +0300

    Initial commit.

---
+++ TelegramBotApp/Global.asax.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Routing;
+using TelegramBotApp.Models;
+
+namespace TelegramBotApp
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected async System.Threading.Tasks.Task Application_StartAsync()
+        {
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            await Bot.Get();
+        }
+    }
+}
---
+++ TelegramBotApp/Models/AppSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TelegramBotApp.Models
+{
+    public static class AppSettings
+    {
+        public static string Url { get; set; } = "https://telegrambotapp.azurewebsites.net:433/{0}";
+
+        public static string Name { get; set; } = "SimpleBot";
+
+        public static string Key { get; set; } = "586941656:AAHhAaJBQtpL6I-uOS0RDATKjgmDfk4Y5XM";
+    }
+}
